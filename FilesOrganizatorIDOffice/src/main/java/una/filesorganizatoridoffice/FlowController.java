e40e6153bc041940f9c7ed57e995c5ae3e94b4e4
moraja1 <jeisonmora0115@gmail.com>
2023-05-03 15:16:39 -0600

    Added styles and starting functionality

--- FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/FlowController.java
+++ FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/FlowController.java
@@ -2,9 +2,12 @@ package una.filesorganizatoridoffice;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import una.filesorganizatoridoffice.viewmodel.WindowController;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class FlowController {
     private Stage mainStage;
@@ -14,6 +17,8 @@ public class FlowController {
         mainStage = stage;
         mainStage.setTitle("Hello!");
         mainStage.setScene(scene);
+        mainStage.getIcons().add(new Image(getClass().getResource("icons/carpet.png").openStream()));
+        mainStage.setResizable(false);
         mainStage.show();
     }
 }
--- FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/viewmodel/WindowController.java
+++ FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/viewmodel/WindowController.java
@@ -5,6 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WindowController {
 
@@ -74,9 +82,39 @@ public class WindowController {
     @FXML
     private Tab tabStudent;
 
-    @FXML
-    void searchBtn_Clicked(ActionEvent event) {
+    private String pdfFileURL;
+    private String photoFileURL;
+    private String excelFileURL;
+    private String outputFileURL;
+    private Integer initialRow;
+    private Integer finalRow;
 
+    @FXML
+    void searchBtn_Clicked(ActionEvent e) {
+        File fileChosen;
+        DirectoryChooser dc = new DirectoryChooser();
+        FileChooser fc = new FileChooser();
+        if(e.getSource() == pdfSearchBtn){
+
+        }
+        if(e.getSource() == photoSearchBtn){
+
+        }
+        if(e.getSource() == excelSearchBtn){
+
+        }
+        if(e.getSource() == outSearchBtn){
+
+        }
+        if(e.getSource() == pdfSearchBtn_Emp){
+
+        }
+        if(e.getSource() == excelSearchBtn_Emp){
+
+        }
+        if(e.getSource() == outSearchBtn_Emp){
+
+        }
     }
 
     @FXML
