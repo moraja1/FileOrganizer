feb464ddb550f5dd9df3bb76ebf23b6abe409cff
moraja1 <jeisonmora0115@gmail.com>
2023-05-04 15:19:39 -0600

    Window functionality completed

--- FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/viewmodel/WindowController.java
+++ FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/viewmodel/WindowController.java
@@ -1,93 +1,142 @@
 package una.filesorganizatoridoffice.viewmodel;
 
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.IntegerBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.File;
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class WindowController {
-
+public class WindowController implements Initializable {
+    @FXML
+    private HBox excelHBox;
     @FXML
     private Button excelSearchBtn;
-
-    @FXML
-    private Button excelSearchBtn_Emp;
-
     @FXML
     private TextField excelTextBox;
-
     @FXML
-    private TextField excelTextBox_Emp;
-
+    private VBox excelVBox;
+    @FXML
+    private VBox excelVBox_Emp;
     @FXML
     private TextField firstRowTextBox;
-
     @FXML
-    private TextField firstRowTextBox_Emp;
-
+    private VBox firstRowVBox;
+    @FXML
+    private VBox firstRowVBox_Emp;
     @FXML
     private TextField lastRowTextBox;
-
     @FXML
-    private TextField lastRowTextBox_Emp;
-
+    private VBox lastRowVBox;
+    @FXML
+    private VBox lastRowVBox_Emp;
+    @FXML
+    private VBox mainVBox;
+    @FXML
+    private VBox mainVBox_Emp;
+    @FXML
+    private HBox outHBox;
     @FXML
     private Button outSearchBtn;
-
-    @FXML
-    private Button outSearchBtn_Emp;
-
     @FXML
     private TextField outTextBox;
-
     @FXML
-    private TextField outTextBox_Emp;
-
+    private VBox outVBox;
+    @FXML
+    private VBox outVBox_Emp;
+    @FXML
+    private HBox pdfHBox;
     @FXML
     private Button pdfSearchBtn;
-
-    @FXML
-    private Button pdfSearchBtn_Emp;
-
     @FXML
     private TextField pdfTextBox;
-
     @FXML
-    private TextField pdfTextBox_Emp;
-
+    private VBox pdfVBox;
+    @FXML
+    private VBox pdfVBox_Emp;
     @FXML
     private Button photoSearchBtn;
-
     @FXML
     private TextField photoTextBox;
-
     @FXML
     private Button startBtn;
-
-    @FXML
-    private Button startBtn_Emp;
-
     @FXML
     private Tab tabEmployee;
-
     @FXML
     private Tab tabStudent;
 
-    private String pdfFileURL = "";
-    private String photoFileURL = "";
-    private String excelFileURL = "";
-    private String outputFileURL = "";
-    private Integer initialRow = 0;
-    private Integer finalRow = initialRow+1;
+    private IntegerProperty initialRow = new SimpleIntegerProperty();
+    private IntegerProperty finalRow = new SimpleIntegerProperty();
+    private final WindowInfo info = new WindowInfo();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        //Change tab event
+        tabStudent.setOnSelectionChanged(event -> {
+            if(tabStudent.isSelected()){
+                pdfVBox.getChildren().add(1, pdfHBox);
+                excelVBox.getChildren().add(1, excelHBox);
+                outVBox.getChildren().add(1, outHBox);
+                firstRowVBox.getChildren().add(1, firstRowTextBox);
+                lastRowVBox.getChildren().add(1, lastRowTextBox);
+                mainVBox.getChildren().add(1, startBtn);
+
+            }else{
+                pdfVBox_Emp.getChildren().add(1, pdfHBox);
+                excelVBox_Emp.getChildren().add(1, excelHBox);
+                outVBox_Emp.getChildren().add(1, outHBox);
+                firstRowVBox_Emp.getChildren().add(1, firstRowTextBox);
+                lastRowVBox_Emp.getChildren().add(1, lastRowTextBox);
+                mainVBox_Emp.getChildren().add(3, startBtn);
+            }
+            clearWindow();
+        });
+
+        //Row number input (Numbers Only)
+        firstRowTextBox.setTextFormatter(getFormatter());
+        lastRowTextBox.setTextFormatter(getFormatter());
+
+        //Binding Properties
+        Bindings.bindBidirectional(firstRowTextBox.textProperty(), initialRow, info.getConverter());
+        Bindings.bindBidirectional(lastRowTextBox.textProperty(), finalRow, info.getConverter());
+
+        info.initialRowProperty().bind(initialRow);
+        info.finalRowProperty().bind(finalRow);
+        info.pdfFileURLProperty().bindBidirectional(pdfTextBox.textProperty());
+        info.outputFileURLProperty().bindBidirectional(outTextBox.textProperty());
+        info.photoFileURLProperty().bindBidirectional(photoTextBox.textProperty());
+        info.excelFileURLProperty().bindBidirectional(excelTextBox.textProperty());
+    }
+
+    private TextFormatter<?> getFormatter() {
+        TextFormatter<String> formatter = new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d*")) {
+                return change;
+            } else {
+                return null;
+            }
+        });
+
+        return formatter;
+    }
 
     @FXML
     void searchBtn_Clicked(ActionEvent e) {
@@ -95,40 +144,27 @@ public class WindowController {
         FileChooser fc = new FileChooser();
         if(e.getSource() == pdfSearchBtn){
             dc.setTitle("Seleccione la carpeta de los PDF");
-            pdfFileURL = verifyBtnInput(e, pdfTextBox, dc);
+            info.setPdfFileURL(verifyBtnInput(e, info.getPdfFileURL(), dc));
         }
         if(e.getSource() == excelSearchBtn){
             fc.setTitle("Seleccione el excel de solicitudes");
-            excelFileURL = verifyBtnInput(e, excelTextBox, fc);
+            info.setExcelFileURL(verifyBtnInput(e, info.getExcelFileURL(), fc));
         }
         if(e.getSource() == outSearchBtn){
             dc.setTitle("Seleccione la carpeta de salida");
-            outputFileURL = verifyBtnInput(e, outTextBox, dc);
+            info.setOutputFileURL(verifyBtnInput(e, info.getOutputFileURL(), dc));
         }
         if(e.getSource() == photoSearchBtn){
             dc.setTitle("Seleccione la carpeta de las fotos");
-            photoFileURL = verifyBtnInput(e, photoTextBox, dc);
+            info.setPhotoFileURL(verifyBtnInput(e, info.getPhotoFileURL(), dc));
         }
-        if(e.getSource() == pdfSearchBtn_Emp){
-            dc.setTitle("Seleccione la carpeta de los PDF");
-            pdfFileURL = verifyBtnInput(e, pdfTextBox_Emp, dc);
-        }
-        if(e.getSource() == excelSearchBtn_Emp){
-            fc.setTitle("Seleccione el excel de solicitudes");
-            excelFileURL = verifyBtnInput(e, excelTextBox_Emp, fc);
-        }
-        if(e.getSource() == outSearchBtn_Emp){
-            dc.setTitle("Seleccione la carpeta de salida");
-            outputFileURL = verifyBtnInput(e, outTextBox_Emp, dc);
-        }
-        uploadWindow();
     }
 
-    public String verifyBtnInput(ActionEvent e, TextField tf, DirectoryChooser dc){
+    public String verifyBtnInput(ActionEvent e, String urlInserted, DirectoryChooser dc){
         File fileChosen;
         String url;
         dc.setInitialDirectory(new File("G:\\Mi unidad"));
-        if(!processInput(tf.getText())){
+        if(!processInput(urlInserted)){
             do {
                 fileChosen = dc.showDialog(new Stage());
             }while (fileChosen == null);
@@ -138,11 +174,11 @@ public class WindowController {
         }
         return correctURL(url);
     }
-    public String verifyBtnInput(ActionEvent e, TextField tf, FileChooser dc){
+    public String verifyBtnInput(ActionEvent e, String urlInserted, FileChooser dc){
         File fileChosen;
         String url;
         dc.setInitialDirectory(new File("C:\\Users\\N00148095\\Downloads"));
-        if(!processInput(tf.getText())){
+        if(!processInput(urlInserted)){
             do {
                 fileChosen = dc.showOpenDialog(new Stage());
             }while(fileChosen == null);
@@ -155,14 +191,11 @@ public class WindowController {
 
     @FXML
     void startBtn_Clicked(ActionEvent event) {
-
+        System.out.println(info.getPdfFileURL());
     }
 
-    private boolean processInput(String text) {
-        if(text.isEmpty()){
-            return false;
-        }
-        File f = new File(text);
+    private boolean processInput(String urlInserted) {
+        File f = new File(urlInserted);
         if(f.exists()){
            return true;
         }
@@ -176,19 +209,12 @@ public class WindowController {
         return url;
     }
 
-    private void uploadWindow() {
-        if(tabStudent.isSelected()){
-            excelTextBox.setText(excelFileURL);
-            pdfTextBox.setText(pdfFileURL);
-            photoTextBox.setText(photoFileURL);
-            outTextBox.setText(outputFileURL);
-        }else{
-            excelTextBox_Emp.setText(excelFileURL);
-            pdfTextBox_Emp.setText(pdfFileURL);
-            outTextBox_Emp.setText(outputFileURL);
-        }
-
+    private void clearWindow(){
+        excelTextBox.clear();
+        pdfTextBox.clear();
+        photoTextBox.clear();
+        outTextBox.clear();
+        firstRowTextBox.clear();
+        lastRowTextBox.clear();
     }
 }
-
-
---
+++ FilesOrganizatorIDOffice/src/main/java/una/filesorganizatoridoffice/viewmodel/WindowInfo.java
@@ -0,0 +1,108 @@
+package una.filesorganizatoridoffice.viewmodel;
+
+import javafx.beans.property.*;
+import javafx.util.StringConverter;
+
+public class WindowInfo {
+    private StringProperty pdfFileURL = new SimpleStringProperty();
+    private StringProperty photoFileURL = new SimpleStringProperty();
+    private StringProperty excelFileURL = new SimpleStringProperty();
+    private StringProperty outputFileURL = new SimpleStringProperty();
+    private IntegerProperty initialRow = new SimpleIntegerProperty();
+    private IntegerProperty finalRow = new SimpleIntegerProperty();
+    private StringConverter<Number> converter;
+
+    public WindowInfo() {
+        converter = new StringConverter<Number>() {
+            @Override
+            public String toString(Number number) {
+                if ((number == null) || (number.intValue() == 0)) {
+                    return "";
+                }
+                return number.toString();
+            }
+            @Override
+            public Number fromString(String s) {
+                if (s == null || s.isEmpty()) {
+                    return null;
+                }
+                return Long.valueOf(s);
+            }
+        };
+    }
+
+    public void setPdfFileURL(String pdfFileURL) {
+        this.pdfFileURL.set(pdfFileURL);
+    }
+
+    public void setPhotoFileURL(String photoFileURL) {
+        this.photoFileURL.set(photoFileURL);
+    }
+
+    public void setExcelFileURL(String excelFileURL) {
+        this.excelFileURL.set(excelFileURL);
+    }
+
+    public void setOutputFileURL(String outputFileURL) {
+        this.outputFileURL.set(outputFileURL);
+    }
+
+    public void setInitialRow(int initialRow) {
+        this.initialRow.set(initialRow);
+    }
+
+    public void setFinalRow(int finalRow) {
+        this.finalRow.set(finalRow);
+    }
+
+    public String getPdfFileURL() {
+        return pdfFileURL.get();
+    }
+
+    public StringProperty pdfFileURLProperty() {
+        return pdfFileURL;
+    }
+
+    public String getPhotoFileURL() {
+        return photoFileURL.get();
+    }
+
+    public StringProperty photoFileURLProperty() {
+        return photoFileURL;
+    }
+
+    public String getExcelFileURL() {
+        return excelFileURL.get();
+    }
+
+    public StringProperty excelFileURLProperty() {
+        return excelFileURL;
+    }
+
+    public String getOutputFileURL() {
+        return outputFileURL.get();
+    }
+
+    public StringProperty outputFileURLProperty() {
+        return outputFileURL;
+    }
+
+    public int getInitialRow() {
+        return initialRow.get();
+    }
+
+    public IntegerProperty initialRowProperty() {
+        return initialRow;
+    }
+
+    public int getFinalRow() {
+        return finalRow.get();
+    }
+
+    public IntegerProperty finalRowProperty() {
+        return finalRow;
+    }
+    public StringConverter<Number> getConverter() {
+        return converter;
+    }
+}
